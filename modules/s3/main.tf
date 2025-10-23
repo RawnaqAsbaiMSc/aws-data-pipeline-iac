b52b8df6acd583e138025ddf8014a787b5129a32
Rawnaq Asbai <Rawnaq.asbai@outlook.com>
2025-10-23 08:16:12 +0100

    Added KMS  module and encryption for bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,10 +2,43 @@ resource "aws_s3_bucket" "raw" {
   bucket = "${var.prefix}-raw-data"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "raw_encryption" {
+  bucket = aws_s3_bucket.raw.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = var.kms_key_arn
+    }
+  }
+}
+
 resource "aws_s3_bucket" "processed" {
   bucket = "${var.prefix}-processed-data"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "processed_encryption" {
+  bucket = aws_s3_bucket.processed.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = var.kms_key_arn
+    }
+  }
+}
+
 resource "aws_s3_bucket" "analytics" {
   bucket = "${var.prefix}-analytics-data"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "analytics_encryption" {
+  bucket = aws_s3_bucket.analytics.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = var.kms_key_arn
+    }
+  }
 }
