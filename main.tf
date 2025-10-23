7ec4fe9ffdf3c5023f0105eaf0d5807867534df0
Rawnaq Asbai <Rawnaq.asbai@outlook.com>
2025-10-23 08:39:48 +0100

    Added Lambda Function Initial

--- main.tf
+++ main.tf
@@ -3,21 +3,26 @@ module "iam_lambda" {
   role_name  = "lambda-etl-role"
   policy_arn = "arn:aws:iam::aws:policy/AWSLambdaBasicExecutionRole"
 }
-
-
-
+module "s3" {
+  source      = "./modules/s3"
+  prefix      = "my-pipeline"
+  kms_key_arn = module.kms.kms_key_arn
+}
+module "kms" {
+  source = "./modules/kms"
+}
 module "observability" {
   source            = "./modules/observability"
   function_name     = "etl-transform"
   retention_in_days = 14
 }
 
-module "kms" {
-  source = "./modules/kms"
+module "lambda" {
+  source           = "./modules/lambda"
+  prefix           = var.prefix
+  lambda_role_arn  = module.iam.lambda_role_arn
+  lambda_zip_path  = "lambda/fetch_api.zip"
+  api_endpoint     = var.api_endpoint
 }
 
-module "s3" {
-  source      = "./modules/s3"
-  prefix      = "my-pipeline"
-  kms_key_arn = module.kms.kms_key_arn
-}
+
